93410a4a50ed1e48f4a8bd7ac8b628c1423769e6
Timothy Dewanto <dewantotims@gmail.com>
2024-02-22 14:52:53 +0700

    sdk34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.satya.native160421048"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.satya.native160421048"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
